ae90fe9c3c8fc97e4db41912495fc16114cb0931
Potti Mokshagna <pottimokshagna@gmail.com>
2026-06-19 15:51:44 +0530

    Debugged Error

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-import Login from '../Login';
 
 function ProtectedRoute({ children }) {
     const jwtToken = Cookies.get('jwt_token');
